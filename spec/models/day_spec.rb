642363ae268e63285d637194df087de21e763836
Maria Ronauli <mronauli@gmail.com>
2020-04-07 17:41:45 -0600

    add day model test

---
+++ spec/models/day_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Day, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:week) }
+    it { should validate_presence_of(:day_of_week) }
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:group) }
+    # it { should have_many(:day_posts) }
+    # it { should have_many(:posts).through(:day_posts) }
+  end
+end
