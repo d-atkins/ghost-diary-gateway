4222fae2c42f23bb4053aa05e331e95f0688c598
Maria Ronauli <mronauli@gmail.com>
2020-04-07 20:08:30 -0600

    add more specific validation to day of week enum

--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Day, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:week) }
     it { should validate_presence_of(:day_of_week) }
-    it { should define_enum_for(:day_of_week) }
+    it { should define_enum_for(:day_of_week).with_values([:Sunday, :Monday, :Tuesday, :Wednesday, :Thursday, :Friday, :Saturday]) }
   end
 
   describe 'relationships' do
