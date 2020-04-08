cd7b126e4af9453c83c47262624f98f2e5b2b0d5
David Atkins <atki1080@gmail.com>
2020-04-08 17:54:42 -0600

    Add home index spec

---
+++ spec/features/home/index_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'Home Index -' do
+  describe 'Connecting with Github -' do
+    before(:each) do
+      @group = Group.create!(name: 'Test Group')
+      User.create!(name: 'other-user', uid: '1111', group: @group)
+
+      auth_mock = {
+        "provider"=>"github",
+        "uid"=> '9999',
+        "info"=> {"nickname"=>"test-user"}
+      }
+
+      OmniAuth.config.mock_auth[:github] = nil
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(auth_mock)
+    end
+
+    describe 'New User' do
+      it 'can create an account with github' do
+        visit '/'
+        expect(page).to have_content('Welcome!')
+        expect(page).to_not have_content('Logged in as ')
+
+        click_link 'Login with GitHub'
+
+        expect(current_path).to eq('/')
+        expect(page).to have_content('Logged in as test-user')
+        expect(page).to_not have_content('Logged in as other-user')
+        expect(page).to_not have_link('Login with GitHub')
+      end
+    end
+
+    describe 'Existing User' do
+      it 'can log in' do
+        User.create!(name: 'test-user', uid: '9999', group: @group)
+        visit '/'
+
+        click_link 'Login with GitHub'
+
+        expect(current_path).to eq('/')
+        expect(page).to have_content('Logged in as test-user')
+        expect(page).to_not have_link('Login with GitHub')
+      end
+    end
+  end
+end
