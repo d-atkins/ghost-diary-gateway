f772e04472a88de985d972e73657fe13cbf438c8
David Atkins <atki1080@gmail.com>
2020-04-08 17:57:31 -0600

    Add sessions controller and create action

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    user_info = request.env['omniauth.auth']
+    uid = user_info[:uid]
+    name = user_info[:info][:nickname]
+
+    user = User.find_by(uid: [uid])
+    if user.nil?
+      user = User.create(uid: uid, name: name, group_id: Group.first.id)
+    end
+    session[:user_id] = user.id
+
+    redirect_to '/'
+  end
+end
