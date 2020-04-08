b94f182f843e63a8bde58c918a5e8793b330fa10
Maria Ronauli <mronauli@gmail.com>
2020-04-07 19:38:43 -0600

    change validation to make sure day of week is an enum

--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Day, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:week) }
-    it { should validate_presence_of(:day_of_week) }
+    it { should define_enum_for(:day_of_week) }
   end
 
   describe 'relationships' do
