028ec7e6b2d092efb60f3add508dfd122ac7d3de
Maria Ronauli <mronauli@gmail.com>
2020-04-07 19:39:17 -0600

    remove default value for day of week

--- db/migrate/20200407232815_create_days.rb
+++ db/migrate/20200407232815_create_days.rb
@@ -2,8 +2,8 @@ class CreateDays < ActiveRecord::Migration[5.2]
   def change
     create_table :days do |t|
       t.references :group, foreign_key: true
-      t.integer :week, default: 0
-      t.integer :day_of_week, default: 0
+      t.integer :week
+      t.integer :day_of_week
 
       t.timestamps
     end
