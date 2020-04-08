f621bbcc915d44deb9204fb83cb61e92f847c35a
Maria Ronauli <mronauli@gmail.com>
2020-04-07 20:58:47 -0600

    change day and post relationship to one to many

--- app/models/day.rb
+++ app/models/day.rb
@@ -2,7 +2,6 @@ class Day < ApplicationRecord
   validates_presence_of :week
   validates_presence_of :day_of_week
   belongs_to :group
-  # has_many :day_posts
-  # has_many :posts, through: :day_posts
+  # has_many :posts
   enum day_of_week: { Sunday: 0, Monday: 1, Tuesday: 2, Wednesday: 3, Thursday: 4, Friday: 5, Saturday: 6 }
 end
