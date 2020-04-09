416cbaba5a112e8bbd976ff955673a281f665ccf
Maria Ronauli <mronauli@gmail.com>
2020-04-09 16:32:25 -0600

    add user post_count test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,20 @@ RSpec.describe User, type: :model do
     it { should have_many(:posts) }
     it { should belong_to(:group) }
   end
+
+  describe 'post_count' do
+    it "can count the amount of posts a user has made" do
+      group_1 = Group.create(name: 'Mod 1')
+      user = User.create(name: 'user', uid: '1111', group: group_1)
+      day_1 = Day.create(week: 1, day_of_week: 1, group: group_1)
+      day_2 = Day.create(week: 2, day_of_week: 5, group: group_1)
+      day_3 = Day.create(week: 4, day_of_week: 3, group: group_1)
+      day_4 = Day.create(week: 6, day_of_week: 4, group: group_1)
+      post_1 = Post.create(body: "Wow, first day of the mod 1... syllabus day... but still so lost @.@", tone: "confuse", day: day_1, user: user)
+      post_2 = Post.create(body: "Just took my first in class challenge. I'm NEVER going to get any better at this!!", tone: "angry", day: day_2, user: user)
+      post_3 = Post.create(body: "Learning that I gotta utilize my resources and getting better doesn't come out of thin air. Make sure to use paired.tech!!", tone: "optimistic", day: day_3, user: user)
+      post_4 = Post.create(body: "Passed Mod 1!! Time to get schwifty with the Turing crew", tone: "excited", day: day_4, user: user)
+      expect(user.post_count).to eq(4)
+    end
+  end
 end
