f05d3fb29962b6b881a851da474393a5bb33fb8b
Maria Ronauli <mronauli@gmail.com>
2020-04-09 16:32:06 -0600

    remove bang from create

--- spec/features/users/user_can_see_its_stats_page_spec.rb
+++ spec/features/users/user_can_see_its_stats_page_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "as a registered user" do
       day_2 = Day.create(week: 2, day_of_week: 5, group: group_1)
       day_3 = Day.create(week: 4, day_of_week: 3, group: group_1)
       day_4 = Day.create(week: 6, day_of_week: 4, group: group_1)
-      post_1 = Post.create!(body: "Wow, first day of the mod 1... syllabus day... but still so lost @.@", tone: "confuse", day: day_1, user: user)
+      post_1 = Post.create(body: "Wow, first day of the mod 1... syllabus day... but still so lost @.@", tone: "confuse", day: day_1, user: user)
       post_2 = Post.create(body: "Just took my first in class challenge. I'm NEVER going to get any better at this!!", tone: "angry", day: day_2, user: user)
       post_3 = Post.create(body: "Learning that I gotta utilize my resources and getting better doesn't come out of thin air. Make sure to use paired.tech!!", tone: "optimistic", day: day_3, user: user)
       post_4 = Post.create(body: "Passed Mod 1!! Time to get schwifty with the Turing crew", tone: "excited", day: day_4, user: user)
