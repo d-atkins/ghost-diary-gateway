42da736c62f568570ef35d133b8e27093d7ab191
Elom Amouzou <elom1990@yahoo.com>
2020-04-10 00:23:12 -0600

    add routes for user show view, stats and posts index views.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/auth/github', as: 'github_login'
   get '/auth/github/callback', to: 'sessions#create'
 
+  get '/profile', to: 'users#show'
+  get '/profile/stats', to: 'user/stats#index'
+  get '/profile/posts', to: 'user/posts#index'
+
   namespace :profile do
     resources :stats, only: [:index]
   end
