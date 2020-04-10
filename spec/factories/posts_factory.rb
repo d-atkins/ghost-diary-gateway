c80be5f22af0c1c1c0cea145f86219337478e14e
David Atkins <atki1080@gmail.com>
2020-04-09 22:32:35 -0600

    Change factory attributes to oneliners
    Simplecov missed lines with the multi-line format

--- spec/factories/posts_factory.rb
+++ spec/factories/posts_factory.rb
@@ -1,16 +1,7 @@
 FactoryBot.define do
   factory :post do
-    body { [Faker::Quote.famous_last_words,
-            Faker::Hacker.say_something_smart,
-            Faker::GreekPhilosophers.quote]
-            .sample.first(222) }
-    tone { ['anger',
-            'fear',
-            'joy',
-            'sadness',
-            'analytical',
-            'confident',
-            'tentative'].sample }
+    body { [Faker::Quote.famous_last_words, Faker::Hacker.say_something_smart, Faker::GreekPhilosophers.quote].sample.first(222) }
+    tone { ['anger', 'fear', 'joy', 'sadness', 'analytical', 'confident', 'tentative'].sample }
     association :day
     association :user
   end
