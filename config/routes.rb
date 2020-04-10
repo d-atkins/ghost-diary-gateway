ca16368f277c4858e10d59f5b5a0eed3941a4603
Elom Amouzou <elom1990@yahoo.com>
2020-04-10 03:08:25 -0600

    add routes for posts index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show'
   get '/profile/stats', to: 'user/stats#index'
   get '/profile/posts', to: 'user/posts#index'
+  get '/profile/posts/:id', to: 'posts#show'
+
+  get '/posts', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
 
   namespace :profile do
     resources :stats, only: [:index]
