d722fd3941dae6697a557fe9319d8133128aab60
Elom Amouzou <elom1990@yahoo.com>
2020-04-10 03:16:08 -0600

    revise controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all.with_matching_group(current_user.group.id)
+    @posts = Post.with_matching_group(current_user.group.id)
   end
 
   def show
