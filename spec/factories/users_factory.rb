91713d4dd4e13ec5d00b07f93d3e490018e9c2b1
David Atkins <atki1080@gmail.com>
2020-04-09 23:40:32 -0600

    Remove group association

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -3,6 +3,5 @@ FactoryBot.define do
     name { "#{Faker::Superhero.descriptor}-#{Faker::Name.first_name}" }
     role { 1 }
     uid { Faker::Number.number(digits: 10).to_s }
-    association :group
   end
 end
