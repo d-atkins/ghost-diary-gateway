37db1499eb65af46ba41d8260b75f61cd23798a7
David Atkins <atki1080@gmail.com>
2020-04-10 19:29:08 -0600

    Remove class methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,15 +4,4 @@ class Post < ApplicationRecord
 
   belongs_to :day
   belongs_to :user
-
-  def self.with_matching_group(user_group_id)
-    all_posts = joins(:day).select('posts.*, days.group_id as group')
-    all_posts.find_all {|post| post.group == user_group_id}
-  end
-
-  def self.matching_group_and_user(user)
-    all_posts = joins(:day).select('posts.*, days.group_id as group')
-    all_posts.find_all {|post| post.group == user.group.id && post.user.id == user.id}
-  end
-
 end
