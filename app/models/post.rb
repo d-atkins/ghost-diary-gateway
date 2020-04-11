0e3c29c6e5d4aff5b702a794865f0fffe8a06625
David Atkins <atki1080@gmail.com>
2020-04-11 15:35:55 -0600

    Add reaction_by_user method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ApplicationRecord
   validates_presence_of :body
   validates_presence_of :tone
 
+  has_many :reactions
   belongs_to :day
   belongs_to :user
+
+  def reaction_by_user(user, category)
+    reactions.find_by(user: user, category: category)
+  end
 end
