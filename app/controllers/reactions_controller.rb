0f7a1edc3593744617b560ea9235dae64cde00e5
David Atkins <atki1080@gmail.com>
2020-04-11 00:51:06 -0600

    Rename type

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,7 +1,7 @@
 class ReactionsController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    Reaction.create(type: params[:type], user: current_user, post: post)
+    Reaction.create(category: params[:category].to_i, user: current_user, post: post)
     redirect_back fallback_location: post_path(post)
   end
 end
