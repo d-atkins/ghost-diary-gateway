d50428690db04aa37377f06bdd58a8df0e668c6b
David Atkins <atki1080@gmail.com>
2020-04-11 13:57:07 -0600

    Add reaction_by_user test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Post, type: :model do
     it { should belong_to(:user) }
   end
 
+  describe 'methods' do
+    it 'reaction_by_user' do
+      user1 = create(:user)
+      user2 = create(:user)
+      post1 = create(:post)
+      post2 = create(:post)
+      reaction1 = create(:reaction, category: 0, post: post1, user: user1)
+      reaction2 = create(:reaction, category: 0, post: post2, user: user2)
+
+      expect(post1.reaction_by_user(user1, 0)).to eq(reaction1)
+      expect(post1.reaction_by_user(user2, 0)).to eq(nil)
+      expect(post1.reaction_by_user(user1, 0)).to eq(nil)
+      expect(post1.reaction_by_user(user2, 0)).to eq(reaction2)
+
+      reaction3 = create(:reaction, category: 0, post: post1, user: user2)
+      expect(post1.reaction_by_user(user2, 0)).to eq(reaction3)
+    end
+  end
 end
