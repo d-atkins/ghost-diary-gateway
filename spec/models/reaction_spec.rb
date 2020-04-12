fc0d79d87ca5737639e6bd3187f06f26cbf938ab
David Atkins <atki1080@gmail.com>
2020-04-11 23:34:56 -0600

    Add opposite test

--- spec/models/reaction_spec.rb
+++ spec/models/reaction_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Reaction, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:post) }
   end
+
+  describe 'methods' do
+    it 'opposite' do
+      post = create(:post)
+      like = create(:reaction, post: post, category: 0)
+      dislike = create(:reaction, post: post, category: 1)
+      can_relate = create(:reaction, post: post, category: 2)
+      lol = create(:reaction, post: post, category: 3)
+      upset = create(:reaction, post: post, category: 4)
+      
+      expect(like.opposite).to eq('dislike')
+      expect(dislike.opposite).to eq('like')
+      expect(can_relate.opposite).to be_nil
+      expect(lol.opposite).to eq('upset')
+      expect(upset.opposite).to eq('lol')
+    end
+  end
 end
