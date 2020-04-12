7e7533d46214fe19c5cfe09550fa050fa9513576
David Atkins <atki1080@gmail.com>
2020-04-11 23:35:04 -0600

    Add opposite method

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -5,4 +5,12 @@ class Reaction < ApplicationRecord
   belongs_to :post
 
   enum category: { like: 0, dislike: 1, can_relate: 2, lol: 3, upset: 4 }
+
+  def opposite
+    return 'dislike' if like?
+    return 'like' if dislike?
+    return nil if can_relate?
+    return 'upset' if lol?
+    return 'lol' if upset?
+  end
 end
