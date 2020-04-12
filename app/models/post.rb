7c606253d190688fd42d23e0b8c4684003dde2a3
David Atkins <atki1080@gmail.com>
2020-04-11 23:55:10 -0600

    Add add_reaction method

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,17 @@ class Post < ApplicationRecord
   def reaction_by_user(user, category)
     reactions.find_by(user: user, category: category)
   end
+
+  def add_reaction(user, category)
+    reaction = Reaction.new(user: user, category: category)
+    reactions << reaction
+    destroy_opposite_reaction(user, reaction)
+  end
+
+  private
+
+    def destroy_opposite_reaction(user, reaction)
+      opposite = reactions.find_by(category: reaction.opposite, user: user)
+      reactions.destroy(opposite) if opposite
+    end
 end
