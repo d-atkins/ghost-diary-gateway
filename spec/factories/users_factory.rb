cc038aac4832fc3ef6b6bd1a15947ea5e1b753e7
David Atkins <atki1080@gmail.com>
2020-04-13 14:37:48 -0600

    Remove spaces from name

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "#{Faker::Superhero.descriptor}-#{Faker::Name.first_name}".downcase }
+    name { "#{Faker::Superhero.descriptor.delete(' ')}-#{Faker::Name.first_name}".downcase }
     role { 1 }
     uid { Faker::Number.number(digits: 10).to_s }
     association :group
