2be2855a539b527b5d8e23b2c4f04c47ccbd71b3
David Atkins <atki1080@gmail.com>
2020-04-13 21:21:33 -0600

    Add number_to_word
    to spell out numbers up to ten

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :category_to_string
+  helper_method :current_user, :category_to_string, :number_to_word
 
   def category_to_string(category)
     ['Like', 'Dislike', 'Can relate', 'Lol', 'Upset'][category]
+  end
+
+  def number_to_word(number)
+    return number if number > 10
+    ['no one',
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+      'ten'][number]
   end
 
   def current_user
