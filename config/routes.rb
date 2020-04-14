a41a44cad4a9b2a51b59e41a21339bbce2ffedbb
Maria Ronauli <mronauli@gmail.com>
2020-04-13 19:41:32 -0600

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get '/auth/github', as: 'github_login'
   get '/auth/github/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   namespace :profile do
     get '/', to: 'dashboard#index'
