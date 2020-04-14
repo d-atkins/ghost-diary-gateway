ab1a0e67db6c1f95f9949101001ea96bfc833504
David Atkins <atki1080@gmail.com>
2020-04-13 22:46:19 -0600

    Move methods to word_helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :category_to_string, :number_to_word
-
-  def category_to_string(category)
-    ['Like', 'Dislike', 'Can relate', 'Lol', 'Upset'][category]
-  end
-
-  def number_to_word(number)
-    return number if number > 10
-    ['no one',
-      'one',
-      'two',
-      'three',
-      'four',
-      'five',
-      'six',
-      'seven',
-      'eight',
-      'nine',
-      'ten'][number]
-  end
+  helper_method :current_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
