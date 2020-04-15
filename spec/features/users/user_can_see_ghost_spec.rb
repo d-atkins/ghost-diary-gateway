e7e04d2fcc4ba42fdfb36dd1adfa440b83ed1375
David Atkins <atki1080@gmail.com>
2020-04-15 12:52:48 -0600

    Use instance variable

--- spec/features/users/user_can_see_ghost_spec.rb
+++ spec/features/users/user_can_see_ghost_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "As a Registered User" do
     group_1 = create(:group, name: 'Mod 1')
     user = create(:user, group: group_1)
     day_1 = create(:day, week: 1, day_of_week: 1, group: group_1)
-    create(:post, day: day_1, user: user)
+    @post = create(:post, day: day_1, user: user)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     allow_any_instance_of(ApplicationController).to receive(:day_today).and_return(day_1)
@@ -20,19 +20,19 @@ RSpec.describe "As a Registered User" do
   it "I can see a ghost on the posts index page" do
     visit "/posts"
 
-    within ".post-#{Post.first.id}" do
+    within ".post-#{@post.id}" do
       expect(page).to have_css(".ghost-container")
     end
   end
 
   it "I can see a ghost on the posts show page" do
-    visit "/posts/#{Post.first.id}"
+    visit "/posts/#{@post.id}"
 
     expect(page).to have_css(".ghost-container")
   end
 
   it "I can see a ghost on my posts show page" do
-    visit "/profile/posts/#{Post.first.id}"
+    visit "/profile/posts/#{@post.id}"
 
     expect(page).to have_css(".ghost-container")
   end
@@ -40,7 +40,7 @@ RSpec.describe "As a Registered User" do
   it "I can see a ghost on my posts index page" do
     visit "/profile/posts"
 
-    within ".post-#{Post.first.id}" do
+    within ".post-#{@post.id}" do
       expect(page).to have_css(".ghost-container")
     end
   end
