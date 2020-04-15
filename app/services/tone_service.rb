366e26d24e3e0754f7cdea34b6233a5c95fa9fba
Maria Ronauli <mronauli@gmail.com>
2020-04-15 11:26:48 -0600

    edit tone service call from ibm to microservice

--- app/services/tone_service.rb
+++ app/services/tone_service.rb
@@ -1,16 +1,17 @@
 class ToneService
 
   def get_tone_by_text(text)
-    response = conn.get("/v3/tone?version=2017-09-21&text=#{text}&sentences=false")
+    response = conn.get("/api/v1/tone") do |request|
+      request.params["body"] = text
+      request.params["apikey"] = ENV["ANALYZER_API_KEY"]
+    end
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
   def conn
-    Faraday.new('https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/95e82454-27aa-4915-a1e6-e206440f077f') do |f|
-      f.use Faraday::Request::BasicAuthentication, 'apikey', ENV["ANALYZER_API_KEY"]
-    end
+    Faraday.new(ENV["TONE_SERVICE_DOMAIN"])
   end
 
 end
