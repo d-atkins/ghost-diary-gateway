f43bf7e9baf2fa5aa21cd215a5188bacbec9c71e
David Atkins <atki1080@gmail.com>
2020-04-16 12:33:05 -0600

    Add display_week test

--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Day, type: :model do
     it { should belong_to(:group) }
     it { should have_many(:posts) }
   end
+
+  describe 'methods' do
+    it 'display_week' do
+      week_1 = create(:day, week: 1)
+      week_2 = create(:day, week: 2)
+      intermission = create(:day, week: 0)
+      vacation = create(:day, week: -1)
+
+      expect(week_1.display_week).to eq('Week 1')
+      expect(week_2.display_week).to eq('Week 2')
+      expect(intermission.display_week).to eq('Intermission Week')
+      expect(vacation.display_week).to eq('Vacation Week')
+    end
+  end
 end
