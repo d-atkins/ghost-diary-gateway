8a069363d70201cd84d225287dce885c43ae91d0
David Atkins <atki1080@gmail.com>
2020-04-16 11:33:42 -0600

    Reroute post create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,29 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    post = Post.new(post_params)
+    if post.save
+      flash[:success] = 'You have made a post!'
+    else
+      flash[:errors] = post.errors.full_messages.to_sentence
+    end
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_body
+      params.permit(:body)
+    end
+
+    def post_params
+      {day: day_today, tone: get_tone, user: current_user}.merge(post_body)
+    end
+
+    def get_tone
+      tone_service = ToneService.new
+      tone_service.get_tone_by_text(post_body)
+    end
+
 end
