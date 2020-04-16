fb59f6cb4a8f5b11b543f721027cf5eea668ac50
David Atkins <atki1080@gmail.com>
2020-04-15 21:26:08 -0600

    Fix logout test

--- spec/features/sessions/destroy_spec.rb
+++ spec/features/sessions/destroy_spec.rb
@@ -7,12 +7,10 @@ describe 'Sessions -' do
       user = create(:user, role: 1, group: group_1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
       visit '/'
-      within '.topnav' do
-        expect(page).to_not have_content("Log in with GitHub")
-        find('#logout').click
-      end
-      expect(current_path).to eq('/')
       expect(page).to_not have_content("Log in with GitHub")
+      find("a[href='#{logout_path}']").click
+      expect(current_path).to eq('/')
+      expect(page).to have_content("You have logged out")
     end
   end
 end
