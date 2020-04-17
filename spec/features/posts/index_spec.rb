eadef732bd5bcc7db631896b7d2e50b4363d433f
David Atkins <atki1080@gmail.com>
2020-04-16 20:53:39 -0600

    Stub faraday response with double

--- spec/features/posts/index_spec.rb
+++ spec/features/posts/index_spec.rb
@@ -14,8 +14,13 @@ RSpec.describe 'Posts index -', type: :feature do
       create(:post, day: day_1, user: user)
       create(:post, day: day_2)
       create(:post, day: day_3, user: user)
+
+      body = File.read('spec/fixtures/events.json')
+      response = double("response", status: 200, body: body)
+
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      allow_any_instance_of(ApplicationController).to receive(:day_today).and_return(day_1)
+      allow_any_instance_of(Time).to receive(:wday).and_return(1)
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_return(response)
 
       visit posts_path
     end
